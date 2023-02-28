f63969b7257a591108c5844a695cdfda64ba7255
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-02-28 14:50:38 +0100

    update register screen

---
+++ app/src/main/java/com/example/barrelaged/biometric/biometricHelper.kt
@@ -0,0 +1,11 @@
+package com.example.barrelaged.biometric
+
+import android.content.Context
+import androidx.biometric.BiometricManager
+
+
+object biometricHelper{
+//    fun hasBiometricOption(context: Context): Int {
+//
+//    }
+}
